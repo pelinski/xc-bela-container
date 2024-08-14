d067911da56a4df0b3eee81e42ac7d419236cad4
Teresa Pelinski <teresapelinski@gmail.com>
2024-08-14 14:55:55 +0100

    speed up building with cache + not rebuilding lib and libraries if not necessary but failing with Pipo

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,22 @@
 FROM debian:bullseye
 ENV DEBIAN_FRONTEND noninteractive
 
-COPY scripts/*.sh scripts/build_settings ./
-COPY CustomMakefile/* /tmp/
+COPY scripts/build_settings ./
 
+COPY scripts/build_packages.sh ./
 RUN ./build_packages.sh && rm build_packages.sh
 
+COPY scripts/build_env.sh ./
 RUN ./build_env.sh && rm build_env.sh
 
+COPY scripts/build_libs.sh ./
 RUN ./build_libs.sh && rm build_libs.sh
 
+COPY CustomMakefile/* /tmp/
+COPY scripts/build_libbelafull.sh ./
 RUN ./build_libbelafull.sh && rm build_libbelafull.sh && rm  tmp/CustomMakefile*
 
+COPY scripts/build_bela.sh ./
 RUN ./build_bela.sh && rm build_bela.sh && rm build_settings
 
 WORKDIR /workspace
--- scripts/build_libs.sh
+++ scripts/build_libs.sh
@@ -3,9 +3,11 @@
 source build_settings
 BBB_ADDRESS=root@$BBB_HOSTNAME
 
-
-ssh $BBB_ADDRESS "cd Bela && make coreclean"
-ssh $BBB_ADDRESS "cd Bela && rm -f lib/*"
 ssh $BBB_ADDRESS "cd Bela && make lib"
-ssh $BBB_ADDRESS "cd Bela && make -f Makefile.libraries cleanall"
 ssh $BBB_ADDRESS "cd Bela && make -f Makefile.libraries all"
+
+# check for clock skew and rebuild if necessary
+ssh $BBB_ADDRESS "cd Bela && make all 2>&1 | tee logfile || exit 1;
+grep -q "skew detected" logfile && { echo CLOCK SKEW DETECTED. CLEANING CORE AND TRYING AGAIN && make coreclean && make lib; } || exit 0;"
+ssh $BBB_ADDRESS "cd Bela && make -f Makefile.libraries all 2>&1 | tee logfile || exit 1;
+grep -q "skew detected" logfile && { echo CLOCK SKEW DETECTED. CLEANING LIBRARIES AND TRYING AGAIN && make -f Makefile.libraries cleanall && make -f Makefile.libraries all; } || exit 0;"
